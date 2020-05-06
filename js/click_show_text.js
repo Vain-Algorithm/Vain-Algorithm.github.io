0397601a9b768c1d871ca388a0ed63b57d6b27e2
嗯呐 否 <993089897@qq.com>
2020-05-06 17:46:50 +0800

    Site updated: 2020-05-06 17:46:47

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
         var a = new Array
-        (" 爱梦娜 ", "      喜欢梦娜", "   梦娜最重要","   我才不是大猪蹄子 ", "   好好学习  ");
+        (" 懒癌患者", "  射手", "  三井寿"," Acmer   ", "   好好学习  ");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
