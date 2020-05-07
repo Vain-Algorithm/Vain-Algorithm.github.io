9e1f70dc01b0e02e5316e8de6401ff87ed7c2b05
嗯呐 否 <993089897@qq.com>
2020-05-07 22:19:58 +0800

    Site updated: 2020-05-07 22:19:56

--- js/click_show_text.js
+++ js/click_show_text.js
@@ -2,7 +2,7 @@ var a_idx = 0;
 jQuery(document).ready(function($) {
     $("body").click(function(e) {
         var a = new Array
-        (" 懒癌患者", "  射手", "  三井寿"," Acmer   ", "   好好学习  ");
+        (" 懒癌患者 ","    爱梦娜","   好好学习  ","   最喜欢梦娜了","   梦娜最棒了","       梦娜拯救世界，我保护梦娜","Acmer");
         var $i = $("<span/>").text(a[a_idx]);
         a_idx = (a_idx + 1) % a.length;
         var x = e.pageX,
